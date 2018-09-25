789d8efe1b602281120453b980802bcb43baff42
Emmanuele <emmanuele.villa@finconsgroup.com>
2018-09-25 07:18:53 +0200

    fixed retrofit

--- ReactiveCraft/common/src/main/java/com/shadowings/reactivecraft/common/core/services/home/CharacterListAPI.java
+++ ReactiveCraft/common/src/main/java/com/shadowings/reactivecraft/common/core/services/home/CharacterListAPI.java
@@ -5,6 +5,7 @@ import com.shadowings.reactivecraft.common.core.models.home.CharacterPreviewList
 import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
@@ -14,8 +15,9 @@ public interface CharacterListAPI {
     Observable<CharacterPreviewList> get();
 
     public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://www.api.myjson.com/")
+            .baseUrl("https://api.myjson.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build();
 
 }
