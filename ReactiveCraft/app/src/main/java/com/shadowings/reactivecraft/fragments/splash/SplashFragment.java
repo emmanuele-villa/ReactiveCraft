7f411615c8493c1c580ef053b012342d058e5deb
Emmanuele <emmanuele.villa@finconsgroup.com>
2018-09-26 14:47:05 +0200

    added home opening

--- ReactiveCraft/app/src/main/java/com/shadowings/reactivecraft/fragments/splash/SplashFragment.java
+++ ReactiveCraft/app/src/main/java/com/shadowings/reactivecraft/fragments/splash/SplashFragment.java
@@ -1,8 +1,13 @@
 package com.shadowings.reactivecraft.fragments.splash;
 
 import com.shadowings.reactivecraft.R;
+import com.shadowings.reactivecraft.common.core.viewmodels.base.ViewModelBase;
+import com.shadowings.reactivecraft.common.core.viewmodels.home.HomeViewModel;
 import com.shadowings.reactivecraft.common.core.viewmodels.splash.SplashViewModel;
 import com.shadowings.reactivecraft.common.droid.fragments.MainSectionFragmentBase;
+import com.shadowings.reactivecraft.fragments.home.HomeFragment;
+
+import java.util.Objects;
 
 public class SplashFragment extends MainSectionFragmentBase<SplashViewModel> {
 
@@ -13,6 +18,21 @@ public class SplashFragment extends MainSectionFragmentBase<SplashViewModel> {
 
     @Override
     protected void registerRules() {
+        register(
+                viewModel
+                        .homeViewModel
+                        .subscribe(this::openHome)
+        );
+    }
 
+    private void openHome(HomeViewModel homeViewModel) {
+        HomeFragment homeFragment = new HomeFragment();
+        homeFragment.setViewModel(homeViewModel);
+
+        Objects.requireNonNull(getActivity())
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, homeFragment)
+                .commit();
     }
 }
